eabe23f30cfb3c77b292f26dcadfed55987efaa3
pengjin0 <827879040@qq.com>
2018-01-04 16:40:05 +0800

    代码c

---
+++ static/config/config.js
@@ -0,0 +1,27 @@
+// 常量 API 接口地址 ===>>>> 正式环境
+//const HOST_CONFIG = 'http://m.builder.artup.com/'
+
+//测试环境
+const HOST_CONFIG = 'http://image2.artup.com/';
+//const HOST_CONFIG = 'http://localhost:8080/';
+
+//图片服务器（静态资源）服务器地址 -===>>>> 正式环境
+//const HOST_STATIC_SERVER_HOST = 'http://m.builder.artup.com/';
+
+//测试环境
+//const HOST_STATIC_SERVER_HOST = 'http://m.builder.artup.com/';
+//const  HOST_STATIC_SERVER_HOST = 'http://img.artup.com/'
+const HOST_STATIC_SERVER_HOST = 'http://image2.artup.com/';
+//const HOST_STATIC_SERVER_HOST = 'http://localhost:8080/';
+
+//赵家福
+const HOST_ZHAOJAIFU = 'http://test2.artup.com/';
+//const HOST_ZHAOJAIFU = 'http://mobile.artup.com/';
+
+//优惠卷正式
+//const COUPON_HOST='http://coupon.artup.com/'
+//优惠劵测试
+const COUPON_HOST='http://testcoupon.artup.com/'
+
+const GET_USERINFO_HOST = 'http://testuser.artup.com'
+
