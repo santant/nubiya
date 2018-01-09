ce59c12b9744d8d37c56af9cf9a192746e3ca9bb
WangYanmin <soocto@gmail.com>
2018-01-09 09:59:00 +0800

    订单详情页面

--- src/components/user/orderDetails.vue
+++ src/components/user/orderDetails.vue
@@ -31,7 +31,8 @@
     <div class="border"></div>
     <div class="order-goods-list">
       <div class="order-goods-item">
-        <div class="thumbnail"></div>
+        <div class="thumbnail"
+             style="background-image: url('https://wx4.sinaimg.cn/mw690/a1019e1ely1fn0d15u54gj20qo1407cl.jpg');"></div>
         <div class="order-goods-info">
           <span class="order-goods-name">未命名未命名</span>
           <span class="order-goods-type">框画</span>
@@ -49,19 +50,19 @@
       <span id="channel-name">努比亚支付</span>
     </div>
     <div class="order-data">
-      <div class="order-number">
+      <div class="line order-number">
         <span>订单编号：2017062759052212169</span>
-        <button class="copy-btn">复制</button>
+        <button class="btn copy-btn">复制</button>
       </div>
-      <div class="coupons">
-        <label for="coupons-amount"></label>
+      <div class="line coupons">
+        <label for="coupons-amount">使用优惠券</label>
         <span id="coupons-amount">-￥89.00</span>
       </div>
-      <div class="order-time">
+      <div class="line order-time">
         <span class="order-time-label">下单时间：</span>
         <span class="order-time-value">2017-06-27 11:16:13</span>
       </div>
-      <div class="order-sum">
+      <div class="line order-sum">
         <div class="row">
           <label for="goods-amount">商品总额</label>
           <span id="goods-amount">￥310</span>
@@ -71,7 +72,7 @@
           <span id="freight">￥0</span>
         </div>
       </div>
-      <div class="actual-payment">
+      <div class="line actual-payment">
         <label for="payment-amount">实付款：</label>
         <span id="payment-amount">￥310.00</span>
       </div>
@@ -115,7 +116,7 @@
       width: 1.5em;
       color: #9698a1;
     }
-    .order-state, .making, .express, .address-info {
+    .order-state, .making, .express, .address-info, .payment-channel {
       padding: 0 .66rem 0 .8rem;
       line-height: 2.4rem;
       background: #fff;
@@ -145,11 +146,13 @@
         color: #999;
         border: 1px solid #c7c7c7;
         background: #fff;
+        outline: 0;
       }
     }
     .address-info {
       padding: 1rem .66rem 1rem .8rem;
       line-height: 1;
+      border: none;
       .address-name, .address-phone {
         font-weight: bold;
       }
@@ -163,6 +166,102 @@
       height: 4px;
       background: url('../../assets/img/border-3l.png');
       background-size: 100% 100%;
+    }
+    .order-goods-list {
+      margin-top: 0.56rem;
+      background: #fff;
+      border: 0 solid #e3e3e3;
+      border-top-width: 1px;
+      border-bottom-width: 1px;
+    }
+    .order-goods-item {
+      display: flex;
+      padding: .48rem .53rem .5rem .88rem;
+    }
+    .thumbnail {
+      flex: 0 0 4.8rem;
+      height: 4.8rem;
+      background-position: 50%;
+      background-repeat: no-repeat;
+      background-size: contain;
+      background-color: #fff;
+      border: 1px solid #ebebeb;
+      border-radius: 4px;
+    }
+    .order-goods-info {
+      flex: 1;
+      width: 100%;
+      padding-left: .88rem;
+      font-size: .64rem;
+      .order-goods-name, .order-goods-type, .order-goods-size, .order-goods-color {
+        display: block;
+        margin-top: .46rem;
+        color: #8a8b96;
+      }
+      .order-goods-name {
+        padding-top: .24rem;
+        margin-top: 0;
+        color: #333;
+      }
+    }
+    .unit-price-count {
+      flex: 0 0 3rem;
+      text-align: right;
+      .unit-price {
+        color: #333;
+      }
+      .count {
+        display: block;
+        margin-top: .46rem;
+        color: #8a8b96;
+      }
+    }
+    .payment-channel {
+      border-bottom: 1px solid #ebebeb;
+      #channel-name {
+        float: right;
+      }
+    }
+    .order-data {
+      margin-top: 0.56rem;
+      color: #999;
+      background: #fff;
+      .line {
+        padding: 0 .66rem 0 .8rem;
+        line-height: 2.4rem;
+        border-bottom: 1px solid #ebebeb;
+      }
+      #coupons-amount, #goods-amount, #freight {
+        float: right;
+      }
+      .actual-payment {
+        border: 0;
+        text-align: right;
+        #payment-amount {
+          color: #d82e00;
+        }
+      }
+      .order-operation {
+        text-align: right;
+      }
+      .btn {
+        margin-left: .6rem;
+        line-height: 1.44rem;
+        width: 5em;
+        text-align: center;
+        border: 1px solid #333;
+        color: #333;
+        background: #fff;
+        outline: 0;
+      }
+      .copy-btn {
+        color: #999;
+        border-color: #999;
+      }
+      .pay-btn, .delete-btn {
+        color: #d82e00;
+        border-color: #d82e00;
+      }
     }
   }
 </style>
